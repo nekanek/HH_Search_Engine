17e101c3fe877d936bc242249a59ad97f364d078
nekanek <inina.marina@gmail.com>
2015-02-13 00:38:33 +0400

    started building search engine

---
+++ src/search/Main.java
@@ -0,0 +1,74 @@
+package search;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        HashMap<Integer, Document> docs = createDocs();
+        HashMap<String, PriorityQueue<Integer>> index = createIndex(docs);
+        
+        
+//        String[] query = getSearchTokens(args);
+        String query="some";
+        System.out.println("Query: " + query);
+        PriorityQueue<Integer> resultsList = findDocuments(query, index);
+        for (Integer i : resultsList) {
+            System.out.println(docs.get(i));
+        }
+        
+    }
+    
+    private static HashMap<String, PriorityQueue<Integer>> createIndex(HashMap<Integer, Document> docs) {
+//        HashMap<String, PriorityQueue<Document>> index = new HashMap<>();
+        HashMap<String, PriorityQueue<Integer>> index = new HashMap<>();
+        
+        // implement tokenizer, stemmer
+            
+        
+// implement comparator by id in Document
+//        PriorityQueue<Document> some = new PriorityQueue<>();
+        PriorityQueue<Integer> some = new PriorityQueue<>();                
+        some.add(1);
+        PriorityQueue<Integer> another = new PriorityQueue<>();                
+        another.add(2);
+        
+        index.put("some", some);     
+        index.put("another", another);
+        return index;
+    }
+    
+    private static String[] getSearchTokens(String[] query) {
+        return query;
+    }
+
+    private static PriorityQueue<Document> findDocuments(String[] query, HashMap<String, PriorityQueue<Document>> index) {
+        PriorityQueue<Document> resultList = new PriorityQueue<>();
+        for (String s : query) {
+            
+            
+        }
+        return resultList;
+    }
+
+    private static HashMap<Integer, Document> createDocs() {
+        HashMap<Integer, Document> docs = new HashMap<>();
+        docs.put(1, new Document(1, "Some quoted text will b here"));
+        docs.put(2, new Document(2, "Another quoted text will b here"));
+        return docs;
+    }
+
+    private static PriorityQueue<Integer> findDocuments(String query, HashMap<String, PriorityQueue<Integer>> index) {
+        PriorityQueue<Integer> results = new PriorityQueue<>();
+        for (String s : index.keySet()) {
+            if (s.equals(query))
+                results.addAll(index.get(s));
+        }
+        return results;
+    }
+    
+}
